9f4fe7e5683b1aa3d436d72da8f1e552f1512711
Rahul Saliya <RHS.Dev@outlook.com>
2024-01-11 14:08:36 -0400

    Initial commit

---
+++ app/src/main/java/com/rhs/waveviewapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.rhs.waveviewapp
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.rhs.waveview.WaveView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val waveView = findViewById<WaveView>(R.id.waveView)
+    }
+}
